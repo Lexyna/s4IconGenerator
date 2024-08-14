09d55236f8a765a56736aa9a6082263a21dc655b
Lexyna <lexieverena@outlook.de>
2024-08-14 12:10:40 +0200

    Added more controll elements to ControllerView

--- src/Components/Controlls.tsx
+++ src/Components/Controlls.tsx
@@ -8,6 +8,32 @@ export const Controlls = () => {
                     <input type="text" placeholder="bgColor#1"></input>
                     <input type="text" placeholder="bgColor#2"></input>
                 </div>
+                <div style={{ display: "flex" }}>
+                    Corner:
+                    <input type="text" placeholder="CornerColor#1"></input>
+                    <input type="text" placeholder="CornerColor#2"></input>
+                </div>
+                <div style={{ display: "flex" }}>
+                    Stripes:
+                    <input type="text" placeholder="stripesColor#1"></input>
+                    <input type="text" placeholder="stripesColor#2"></input>
+                </div>
+                <div style={{ display: "flex" }}>
+                    Emblem:
+                    <select>
+                        <option value="Circle">Circle</option>
+                    </select>
+                    <input type="text" placeholder="emblemColor#1"></input>
+                    <input type="text" placeholder="emblemColor#2"></input>
+                </div>
+                <div style={{ display: "flex" }}>
+                    Symbol:
+                    <select>
+                        <option value="SZ">SZ</option>
+                    </select>
+                    <input type="text" placeholder="symbolColor#1"></input>
+                    <input type="text" placeholder="symbolColor#2"></input>
+                </div>
             </div>
         </>
     )
