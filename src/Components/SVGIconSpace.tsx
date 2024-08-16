c36d6005ba5fa9b687f69d83d485e33dd4f86030
Lexyna <lexieverena@outlook.de>
2024-08-16 14:15:04 +0200

    Reverted s4 logo s back to s type

--- src/Components/SVGIconSpace.tsx
+++ src/Components/SVGIconSpace.tsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { leaning_One, normal4, path_originalS, path_Z, stylish3, stylish4 } from "./SVGSymbolPaths";
+import { leaning_One, normal4, path_Z, stylish3, stylish4 } from "./SVGSymbolPaths";
 
 export interface SVGIconProps {
     bgColor1: string,
@@ -262,7 +262,7 @@ export const SVGIconSpace = (props: SVGIconProps) => {
                             d={stylish4}
                             filter="drop-shadow(3px 5px 2px rgb(0 0 0 / 0.4))" transform="translate(-4 -10)" />
                         <path strokeWidth="4" fill="url(#SymbolGradient)"
-                            d={path_originalS}
+                            d={props.SType}
                             filter="drop-shadow(3px 5px 2px rgb(0 0 0 / 0.4))" transform="translate(4 2)" />
                     </> :
                     <></>}
